ce51888c172a82ff84a8cd4531e9e49c0896af00
BassimBenarab <babe0002@stud.kea.dk>
2025-11-27 03:36:23 +0100

    chore: intial project commit

---
+++ src/main/java/dk/kea/alpha_projectcalculator/AlphaProjectCalculatorApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.alpha_projectcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaProjectCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaProjectCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/alpha_projectcalculator/AlphaProjectCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.alpha_projectcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaProjectCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
